43613d9c825d5ab38d35037a23463229471c399e
Nitya-11 <NITYASINGH559@GMAIL.COM>
2025-07-05 23:26:31 +0530

    Add Js code

---
+++ app.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_SS60ndNPYbRKvT3nOE0a4DGicyxRWSbWXdbWU1PE";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropdown) {
+    for(currCode in countryList) {
+        let newOption = 
+    }
+}
